c5c1e746ab8fe755b8af9b42104a8a29b27ae27d
fhdzleon <fhdzleon@gmail.com>
2025-01-15 22:27:08 -0600

    chore(fhdzleon): :wrench: add middelware and configure tailwind
    Rutas protegidas con el middleware que revisara el token en cookies, se agrego el plugin de daisyui a tailwind config y se agregaron los colores de la UI Kit de diseño

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -1,7 +1,16 @@
 function LandingPage() {
   return (
+    //Pruebas de tailwindConfig Borrarlo al renderizar los componentes -->
+
     <div>
       <span>landing page</span>
+      <p className="text-primary">primary</p>
+      <p className="text-textPrimary">Texto</p>
+      <p className="text-alert">alert</p>
+      <p className="text-success">Succes</p>
+      <p className="text-warning">Warning</p>
+
+      <button className="btn btn-primary">Botón de DaisyUI</button>
     </div>
   );
 }
