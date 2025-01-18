609d31395cf8efb07059716750e72a89a294d610
samuelbernal44 <samuelbernal44@gmail.com>
2025-01-17 23:30:47 -0500

    feat(app): add CORS configuration to allow cross-origin requests

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,37 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Configurar CORS
+const allowedOrigin = [
+  // 'https://s17-09-n-node-react-2.onrender.com', link deploy
+  // 'https://s17-09-n-node-react.onrender.com',
+  // 'http://localhost:5173', ruta local
+  process.env.CLIENT_URL, // Usar una variable de entorno para la URL del cliente
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigin.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,POST,PUT,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Content-Disposition',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Credentials',
+    ],
+  })
+);
+
 // Swagger
 import setupSwaggerV1 from '../swagger/v1/main.js';
 setupSwaggerV1(app);
