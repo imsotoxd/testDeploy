4f2b1c181abd1c4d72b576960227596b046f6b21
samuelbernal44 <samuelbernal44@gmail.com>
2025-01-28 22:26:29 -0500

    feat: validación en endpoint para consultar o modificar productos creados por usuario autenticado y cambios en documentación

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -46,7 +46,8 @@ export const createProduct = async (req, res) => {
 // Obtener la lista de todos los productos
 export const getAllProducts = async (req, res) => {
   try {
-    const products = await getAllProductsService();
+    const userId = req.userId;
+    const products = await getAllProductsService(userId);
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: 'Error interno del servidor', error });
@@ -56,10 +57,11 @@ export const getAllProducts = async (req, res) => {
 // Obtener los detalles de un producto por su ID
 export const getProductById = async (req, res) => {
   try {
+    const userId = req.userId;
     const { id } = req.params;
     const product = await getProductByIdService(id);
-    if (!product) {
-      return res.status(404).json({ message: 'Producto no encontrado' });
+    if (!product || product.userId !== userId) {
+      return res.status(403).json({ message: 'No tienes permiso para acceder a este producto.' });
     }
     res.status(200).json(product);
   } catch (error) {
@@ -70,61 +72,79 @@ export const getProductById = async (req, res) => {
 // Actualizar la información de un producto existente
 export const updateProduct = async (req, res) => {
   try {
+    const userId = req.userId;
     const { id } = req.params;
-    const updatedProduct = await updateProductService(id, req.body);
+    const updatedProduct = await updateProductService(userId, id, req.body);
     res.status(200).json({
       message: 'Producto actualizado con éxito',
       product: updatedProduct,
     });
-  } catch (error) {
-    res.status(500).json({ message: 'Error interno del servidor', error });
+  }  catch (error) {
+    if (error.message === 'Producto no encontrado o no tienes permiso para actualizarlo') {
+      res.status(403).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: 'Error interno del servidor', error });
+    }
   }
 };
 
 // Eliminar un producto (soft delete)
 export const deleteProduct = async (req, res) => {
   try {
+    const userId = req.userId;
     const { id } = req.params;
-    const deletedProduct = await deleteProductService(id);
+    const deletedProduct = await deleteProductService(userId, id);
     res.status(200).json({
       message: 'El producto se eliminó con éxito',
       product: deletedProduct,
     });
   } catch (error) {
-    res.status(500).json({ message: 'Error interno del servidor', error });
+    if (error.message === 'Producto no encontrado o no tienes permiso para eliminarlo') {
+      res.status(403).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: 'Error interno del servidor', error });
+    }
   }
 };
 
 // Restaurar un producto eliminado
 export const restoreProduct = async (req, res) => {
   try {
+    const userId = req.userId;
     const { id } = req.params;
-    const restoredProduct = await restoreProductService(id);
+    const restoredProduct = await restoreProductService(userId, id);
     res.status(200).json({
       message: 'Producto restaurado con éxito',
       product: restoredProduct,
     });
   } catch (error) {
-    res.status(500).json({ message: 'Error interno del servidor', error });
+    if (error.message === 'Producto no encontrado o no tienes permiso para restaurarlo') {
+      res.status(403).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: 'Error interno del servidor', error });
+    }
   }
 };
 
 // Nuevo controlador para consultas de filtrado y ordenamiento
 export const queryProducts = async (req, res) => {
   try {
+    const userId = req.userId;    
     const filter = req.query.filter || {};
     const sort = req.query.sort || {};
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
 
-    const { products, totalQuantity, totalAvailableQuantity, totalItems } =
-      await queryProductsService(filter, sort, page, limit);
+    const { products, totalQuantity, totalAvailableQuantity, totalItems, currentPage, totalPages } =
+      await queryProductsService(userId, filter, sort, page, limit);
 
     const response = {
       products,
       totalQuantity, // Cantidad total de la página actual
       totalAvailableQuantity, // Cantidad total disponible de todos los productos sin importar la paginación
       totalItems, // Total de productos disponibles sin importar la paginación
+      currentPage,
+      totalPages,
       page,
       limit,
     };
