7eb2f52e372ebeb18f09527be949851741f4fea9
fhdzleon <fhdzleon@gmail.com>
2025-01-31 01:59:12 -0600

    now

--- frontend/src/lib/tanstack/prefetchers.ts
+++ frontend/src/lib/tanstack/prefetchers.ts
@@ -3,12 +3,11 @@ import { getAllProducts } from "@/app/api/product.api";
 import { getAllCategories } from "@/app/api/categories.api";
 
 export async function prefetchProducts(queryClient: QueryClient) {
-  // @ts-ignore
   await queryClient.prefetchQuery({
     queryKey: ["products"],
     queryFn: getAllProducts,
   });
-  // @ts-ignore
+
   await queryClient.prefetchQuery({
     queryKey: ["categories"],
     queryFn: getAllCategories,
