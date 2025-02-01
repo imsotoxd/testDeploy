20395ac9653604140efe04661d097476aeb28840
Edu Moreno <e.m.morenolp@gmail.com>
2025-02-01 13:19:43 -0300

    creacion de archivos movement

---
+++ Backend/src/routes/movementRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { createMovementController } from '../controllers/movementController';
+import { validateMovement } from '../middleware/validation/categoryValidation.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
+import { handleValidationErrors } from '../middleware/handleValidationErrors.js';
+
+
+const router = express.Router();
+
+router.post('/movement', authenticateToken, createMovementController);
+router.get('/movement', authenticateToken, getMovementController);
+router.get('/movement/:id', authenticateToken, getIdMovementController);
+router.put('/movements/update/:id', authenticateToken, updateMovementController);
+router.delete('/movements/delete/:id', authenticateToken, deleteMovementController);
+router.get('/movements/product/:productId', authenticateToken, productMovementController);
+
+export default router;
